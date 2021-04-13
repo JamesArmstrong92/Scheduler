16feda589a4220fafb48663fac173f2a7d0f85de
JamesArmstrong92 <jarmst23@gmail.com>
2021-04-13 15:16:26 -0400

    Added InterviewerList and InterviewerListItem Components

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,19 +1,26 @@
 import React from "react";
-import 'components/InterviewerListItem.scss';
+import "components/InterviewerListItem.scss";
 import classNames from "classnames";
 
 
 
 export default function InterviewListItem(props) {
 
+  const className = classNames({
+    'interviewer-list__item': true,
+    'interviewer-list__item--selected': props.selected,
+
+  })
+
   
 
-  return (<li className="interviewers__item">
+  return (<li className={className} onClick={props.setInterviewer}>
   <img
     className="interviewers__item-image"
-    src="https://i.imgur.com/LpaY82x.png"
-    alt="Sylvia Palmer"
+    src={props.avatar}
+    alt={props.name}
   />
-  Sylvia Palmer
+  
+  {props.selected ? props.name : ""} 
 </li>)
 };
