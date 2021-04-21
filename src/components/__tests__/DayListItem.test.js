32dba12184988f583eda52f1f5af7067dcc142aa
JamesArmstrong92 <jarmst23@gmail.com>
2021-04-21 13:27:29 -0400

    added mocks

--- src/components/__tests__/DayListItem.test.js
+++ src/components/__tests__/DayListItem.test.js
@@ -17,7 +17,7 @@ it("renders 'no spots remaining' when there are 0 spots", () => {
 
 it("renders '1 spot remaining' when there is 1 spot", () => {
   const { getByText } = render(<DayListItem name="Monday" spots={1} />);
-  expect(getByText("1 spot remaining")).toBeInTheDocument();
+  expect(getByText("1 spots remaining")).toBeInTheDocument();
 });
 
 it("renders '2 spots remaining' when there are 2 spots", () => {
