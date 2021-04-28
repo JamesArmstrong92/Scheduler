c279cd1f62a3e802c9f4adb2179011855ab103b3
JamesArmstrong92 <jarmst23@gmail.com>
2021-04-28 14:17:24 -0400

    Small bug fixes

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,34 +1,29 @@
 import React from "react";
-import "components/DayListItem.scss";
+import "components/DayListItem.scss"
 import classNames from "classnames";
+
 
 export default function DayListItem(props) {
 
-  
-
+  //Outputs spots remaining
   const formatSpots = function(props) {
-      
+    if (props.spots === 1) return `${props.spots} spots remaining`;
 
-    if (props.spots === 1 )  return `${props.spots} spot remaining`;
-    
-    if (props.spots > 1) return `${props.spots} spots remaining`;
-     
-        return `no spots remaining`
-    
+    if (props.spots > 1) return `${props.spots} spot remaining`;
+
+    return `0 spots remaining`
   };
 
-  const dayClassNames = classNames({
-
+  const dayClassName = classNames({
     'day-list__item': true,
     'day-list__item--selected': props.selected,
     'day-list__item--full': !props.spots
-
   });
 
   return (
-    <li className={dayClassNames} onClick={() => props.setDay(props.name)} data-testid="day">
-      <h2 className="text--regular">{props.name}</h2>
-    {formatSpots(props)}
+    <li className={dayClassName} onClick={() => props.setDay(props.name)} data-testid="day">
+      <h2>{props.name}</h2>
+      {formatSpots(props)}
     </li>
   );
-}
+};
